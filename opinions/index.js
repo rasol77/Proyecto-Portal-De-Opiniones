32572392e81cb38813bf1127ef4b8e5e071bcaac
Carlos <58401176+rasol77@users.noreply.github.com>
2022-06-05 18:36:47 +0200

    Delete opinions directory

--- opinions/index.js
+++
@@ -1,4 +0,0 @@
-const getOpinion = require('./getOpinion');
-const listOpinions = require('./listOpinions');
-const newOpinion = require('./newOpinion');
-module.exports = { newOpinion, listOpinions, getOpinion };
